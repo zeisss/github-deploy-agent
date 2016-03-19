2f04bcfb3760ed343a43cc1489f2bf8dfedafe22
Stephan Zeissler <stephan@giantswarm.io>
2016-03-19 14:10:48 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,60 @@
+# github-deploy-agent
+
+The github-deploy-agent is a simple go agent that periodically polls the github api and checks for new deployments.
+If found, it executes a hook to deploy.
+
+## Workflow
+
+In my case I use [wercker](https://wercker.com) to create a deployment object in the repository.
+
+The agent periodically polls this repository and looks for the latest deployment.
+If a newer than previously deployed exists, it starts deploying it.
+
+ * It creates a deployment status `pending`.
+ * A `pre_task` is fired, if available.
+ * If tries executing a file called `./hooks/$task`, where `$task` is the string
+   from the deployment object. This allows for different actions to be taken.
+
+   The hook gets passed a number of environment variables, including `GITHUB_REF` and `GITHUB_SHA`
+   which define which branch/commit of the repository should be commited.
+
+   This hook, which can be anything (bash scripts, binaries etc.) can now perform the actual deploy.
+
+ * The agent creates a `success` status if the exit code is `0`, `failure` otherwise.
+ * A `post_success` hook is fired on success with the same environment variables,
+   a `post_failure` otherwise. Errors here are ignored.
+
+   This can be used for slack notifications etc.
+ * The agent sleeps for `--sleep` before checking for the next deployment again.
+
+## Environment Variables
+
+All hooks are passed the same environment variables.
+
+Name                   | Desc
+-----------------------|--------------------
+GITHUB_TASK            | The task name
+GITHUB_DEPLOYMENT_ID   | ID of the deployment
+GITHUB_DEPLOYMENT_URL  | URL of the deployment object in the Github API
+GITHUB_REF             | Branch name to be deployed
+GITHUB_SHA             | SHA of the commit to be deployed
+GITHUB_OWNER           | Owner of the repository
+GITHUB_REPO            | Repository name
+GITHUB_CREATOR         | Name of the user that created the deployment
+GITHUB_CREATOR_AVATAR  | Avatar URL of the creator
+
+## Caveats
+
+1. On startup, the agent always redeploys the latest deployment found in the repository. It is up
+   to the hook to ignore redeploys of the latest version, if wanted.
+
+2. If multiple deployments are created for the same environment while the agent sleeps,
+   only the latest is acted upon. If the deployments differ e.g. in the tasks to execute,
+   this may lead to unexpected / confusing behavior.
+
+3. Although the agent is single threaded (and thus only applies one deploy at a time), it
+   does not help you with downtime-free rollouts etc.
+
+## Future Improvements
+* Github Deployment API supports a payload. This is currently not made available to the hook.
+* Expose the output of the hooks, e.g. by uploading them to Gist.
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+  "time"
+  "fmt"
+  "os"
+  "os/exec"
+  "strings"
+  "log"
+
+  "github.com/google/go-github/github"
+  "github.com/spf13/pflag"
+  "golang.org/x/oauth2"
+)
+
+var (
+  repository = pflag.String("repository", "", "The repository to deploy for - owner/repo")
+  env = pflag.String("env", "production", "The environment to act on")
+  token = pflag.String("token", "", "API token for github")
+  sleepTime = pflag.Duration("sleep", 60 * time.Second, "Sleep time between checks to github api")
+)
+func main() {
+  pflag.Parse()
+
+  ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *token},
+	)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+
+  client := github.NewClient(tc)
+
+  s := strings.Split(*repository, "/")
+  owner := s[0]
+  repo := s[1]
+
+  var err error
+  var minID int
+  for {
+    if minID, err = checkRepo(client, owner, repo, minID); err != nil {
+      panic(err.Error())
+    }
+    time.Sleep(*sleepTime)
+  }
+}
+
+func checkRepo(client *github.Client, owner, repo string, minID int) (int, error) {
+  deployments, _, err := client.Repositories.ListDeployments(owner, repo, &github.DeploymentsListOptions{
+    Environment: *env,
+  })
+  if err != nil {
+    return -1, err
+  }
+
+  var newestDeployment *github.Deployment
+  for _, deployment := range deployments {
+    if *deployment.ID <= minID {
+      continue
+    }
+    if newestDeployment == nil || deployment.CreatedAt.Time.After(newestDeployment.CreatedAt.Time) {
+      newestDeployment = &deployment
+    }
+  }
+
+  if newestDeployment == nil {
+    log.Printf("No new deployments found.\n")
+    return minID, nil
+
+  }
+
+  log.Printf("Deploying %d\n", *newestDeployment.ID)
+  if err := deploy(client, owner, repo, newestDeployment); err != nil {
+    return -1, err
+  }
+  return *newestDeployment.ID, nil
+}
+
+func deploy(client *github.Client, owner, repo string, depl *github.Deployment) error {
+  if err := createDeploymentStatus(client, owner, repo, depl, "pending", "Firing deployment hook"); err != nil {
+    return nil
+  }
+
+  env := []string{
+    fmt.Sprintf("GITHUB_TASK=%s", *depl.Task),
+    fmt.Sprintf("GITHUB_DEPLOYMENT_ID=%d", *depl.ID),
+    fmt.Sprintf("GITHUB_DEPLOYMENT_URL=%s", *depl.URL),
+    fmt.Sprintf("GITHUB_OWNER=%s", owner),
+    fmt.Sprintf("GITHUB_REPO=%s", repo),
+    fmt.Sprintf("GITHUB_REF=%s", *depl.Ref),
+    fmt.Sprintf("GITHUB_SHA=%s", *depl.SHA),
+    fmt.Sprintf("GITHUB_CREATOR=%s", *depl.Creator.Login),
+    fmt.Sprintf("GITHUB_CREATOR_AVATAR=%s", *depl.Creator.AvatarURL),
+  }
+
+  if _, err := fireHook("pre_task", env); err != nil {
+    log.Printf("pre_task failed: %v\n", err)
+  }
+
+  if found, err := fireHook(*depl.Task, env); err != nil {
+    if err := createDeploymentStatus(client, owner, repo, depl, "failure", "Hook failed"); err != nil {
+      return nil
+    }
+
+    if _, err := fireHook("post_failure", env); err != nil {
+      log.Printf("post_failure failed: %v\n", err)
+    }
+  } else if !found {
+    log.Printf("No hook '%s' found.\n", *depl.Task)
+    if err := createDeploymentStatus(client, owner, repo, depl, "failure", "Unknown hook: " + *depl.Task); err != nil {
+      return nil
+    }
+  } else {
+    if err := createDeploymentStatus(client, owner, repo, depl, "success", "Finished"); err != nil {
+      return nil
+    }
+
+    if _, err := fireHook("post_success", env); err != nil {
+      log.Printf("post_success failed: %v\n", err)
+    }
+  }
+  return nil
+}
+
+func createDeploymentStatus(client *github.Client, owner, repo string,  depl *github.Deployment, state, desc string) error {
+  log.Printf("Setting state=%s descr=%s\n", state, desc)
+  _, _, err := client.Repositories.CreateDeploymentStatus(owner, repo, *depl.ID, &github.DeploymentStatusRequest{
+    State: &state,
+    Description: &desc,
+  })
+  return err
+}
+
+func fireHook(name string, env []string) (bool, error) {
+  _, err := os.Stat("./hooks/" + name)
+  if err != nil {
+    if os.IsNotExist(err) {
+      return false, nil
+    }
+    return false, err
+  }
+
+  log.Printf("Firing hook %s\n", name)
+  cmd := exec.Command("./hooks/" + name)
+  cmd.Env = env
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+  return true, cmd.Run()
+}
